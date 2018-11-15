1de74d97c345fd553c98a76b0f04091bb617a3d8
Lianne Hufkens <lianne_hufkens@yahoo.com>
2018-11-15 18:06:52 +0100

    Added midpoint/crossing to edges.

--- src/main/java/svg/Edge.java
+++ src/main/java/svg/Edge.java
@@ -5,10 +5,17 @@ import org.locationtech.jts.geom.Coordinate;
 public class Edge {
     public Coordinate c1;
     public Coordinate c2;
+    public Coordinate midpoint;
 
     public Edge(Coordinate c1, Coordinate c2){
         this.c1 = c1;
         this.c2 = c2;
+        this.midpoint = getCrossing();
+    }
+
+    private Coordinate getCrossing()
+    {
+        return new Coordinate(Math.round((this.c1.x + this.c2.x) / 2d), Math.round((this.c1.y + this.c2.y) / 2d));
     }
 
     public Boolean equals(Edge other){
@@ -19,6 +26,6 @@ public class Edge {
     }
 
     public String toString(){
-        return "C1: " + c1.toString() + ", C2: " + c2.toString();
+        return "C1: " + c1.toString() + ", C2: " + c2.toString() + ", M: " + midpoint.toString();
     }
 }
